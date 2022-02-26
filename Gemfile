996dd7017324c89c16131f6b4eaf3f6de8b47e49
umlautone uenishi <uenishi@umlautone.jp>
2022-02-26 22:51:57 +0900

    rubocopのエラーを解消

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
-
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
+
+group :development do
+  gem 'rubocop-fjord', require: false
+end
--- app.rb
+++ app.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
-require "json"
-
+require 'json'
 
 get '/' do
-  redirect to('/memos/') 
+  redirect to('/memos/')
 end
 
 # メモ一覧画面
 get '/memos/' do
   @memos = Dir.glob('db/*.json').map do |file|
-    JSON.parse(File.open(file,"r").read)
+    JSON.parse(File.open(file, 'r').read)
   end
-  @memos = @memos.sort_by{ |file| file['id']}.reverse
+  @memos = @memos.sort_by { |file| file['id'] }.reverse
   erb :index
 end
 
@@ -24,48 +25,44 @@ end
 # メモ作成
 post '/memos/' do
   # メモの内容をローカル変数に入れる
-  time = Time.now.strftime("memo_%Y%m%d%H%M%S")
-  memo = {id: time, title: params[:title], body: params[:body]}
+  time = Time.now.strftime('memo_%Y%m%d%H%M%S')
+  memo = { id: time, title: params[:title], body: params[:body] }
   # ローカル変数の内容をファイルを新規作成して入れる
-  File.open("db/#{time}.json", "w") do |file|
+  File.open("db/#{time}.json", 'w') do |file|
     JSON.dump(memo, file)
   end
-  redirect to('/memos/') 
+  redirect to('/memos/')
 end
 
 # メモ詳細画面
 get '/memos/:id' do
-  file_url = "db/#{params['id']}.json"
-  @memo = JSON.parse(File.open(file_url,"r").read)
+  file_url = "db/#{params[:id]}.json"
+  @memo = JSON.parse(File.open(file_url, 'r').read)
   erb :show
 end
 
 # メモ変更画面表示
 get '/memos/:id/edit' do
-  file_url = "db/#{params['id']}.json"
-  @memo = JSON.parse(File.open(file_url,"r").read)
+  file_url = "db/#{params[:id]}.json"
+  @memo = JSON.parse(File.open(file_url, 'r').read)
   erb :edit
 end
 
 # メモ変更
 patch '/memos/:id/edit' do
-  file_url = "db/#{params['id']}.json"
+  file_url = "db/#{params[:id]}.json"
   # メモの内容をローカル変数に入れる
-  memo = {id: params[:id], title: params[:title], body: params[:body]}
+  memo = { id: params[:id], title: params[:title], body: params[:body] }
   # ローカル変数の内容でファイルを上書きする
-  File.open(file_url, "w") do |file|
+  File.open(file_url, 'w') do |file|
     JSON.dump(memo, file)
   end
-  redirect to('/memos/') 
+  redirect to('/memos/')
 end
 
 # メモ削除
 delete '/memos/:id' do
-  file_url = "db/#{params['id']}.json"
-  begin
-    File.delete(file_url)
-    redirect to('/memos/') 
-  rescue
-    p 'ファイルを削除できませんでした'
-  end
+  file_url = "db/#{params[:id]}.json"
+  File.delete(file_url)
+  redirect to('/memos/')
 end
